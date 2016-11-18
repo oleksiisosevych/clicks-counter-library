520360eca35d85dc8a5a672aa4389db9c5fea063
Oleksii Sosevych <oleksiisosevych@gmail.com>
2016-11-18 07:52:35 +0100

    Initial empty project commit

---
+++ app/src/main/java/com/oleksiisosevych/flickrimagesbrowsermvp/StartActivity.java
@@ -0,0 +1,13 @@
+package com.oleksiisosevych.flickrimagesbrowsermvp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+    }
+}
